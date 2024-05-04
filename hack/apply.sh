05ba6c405c53738df6dc743fd74bad89f11791bf
jack munday <62025739+1602077@users.noreply.github.com>
2024-05-04 22:40:43 +0200

    build: provide region options in deployment script

--- hack/apply.sh
+++ hack/apply.sh
@@ -1,19 +1,36 @@
-#!/bin/bash
+#!/bin/zsh
 # runs terraform apply to deploy vm.
+
+print-bold() {echo -e "\033[1m$@\033[0m"}
+
+declare -A GCP_REGIONS
+GCP_REGIONS=(
+	[Berlin]=europe-west10
+	[London]=europe-west2
+	[Oregon]=us-west1
+	[Paris]=europe-west9
+	[Sydney]=australia-southeast1
+	[Zurich]=europe-west6
+)
+declare -a GCP_PROJECTS=($(gcloud projects list | awk '{print $1}' | tail -n +2))
+
+print-bold ">> Creating VPN"
+echo -e "Available projects: ${GCP_PROJECTS}"
+vared -p "Enter project name: " -c GCP_PROJECT_NAME
+echo -e "Available cities: ${(k)GCP_REGIONS}"
+
+# If no default region passed by environment variable prompt for input.
+if [[ -z $GCP_REGION ]]; then
+  vared -p "Enter a city: " -c GCP_REGION
+fi
 
 IPSEC_PSK=${IPSEC_PSK:=$RANDOM$RANDOM$RANDOM$RANDOM}
 IPSEC_USERNAME=${IPSEC_USERNAME:="vpnuser"}
-IPSEC_PASSWORD=${IPSEC_PASSWORD:=$(LC_ALL=C tr -dc A-Za-z0-9_ </dev/urandom | head -c 20 | tr -d '\n')}
+IPSEC_PASSWORD=${IPSEC_PASSWORD:=$(LC_ALL=C tr -dc A-Za-z0-9_ </dev/urandom | head -c 20 | tr -d "\n")}
 GCP_VM_HOSTNAME=${GCP_VM_HOSTNAME:="vpn-001"}
 GCP_REGION=${GCP_REGION:="europe-west2"}
 GCP_ZONE=${GCP_ZONE:="europe-west2-c"}
 
-gcloud projects list
-echo ""
-
-# GCP_PROJECT_NAME is the only variable where a sensible default can not be
-# set: prompt from user.
-read -p "Enter gcp project name: " GCP_PROJECT_NAME
 
 (
 	cd terraform/build
@@ -27,6 +44,7 @@ read -p "Enter gcp project name: " GCP_PROJECT_NAME
 		-auto-approve
 )
 
+print-bold "VPN created succesfully - you may need to wait up to 10 minutes for startup script to finish\n"
 echo "Create a CISCO IPSEC VPN with the below config to get started:"
 echo "GCP VM EXTERNAL IP: $(gcloud compute instances describe $GCP_VM_HOSTNAME --format='get(networkInterfaces[0].accessConfigs[0].natIP)' --zone $GCP_ZONE)"
 echo "IPSEC USERNAME: $IPSEC_USERNAME"
